3029395712e9ee16071c7ed72fc3cf8d2fe32202
VladyslavSarbash <79258970+VladyslavSarbash@users.noreply.github.com>
2021-08-07 22:38:50 +0300

    'HW'

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,13 @@ import arrayMenu from '../menu.json';
 import menuElTpl from '../templates/menu-item.hbs';
 import addClassTheme from './cheangeTheme.js';
 import localStorageTheme from './localStorageTheme';
-import theme from '../theme.json';
 
-const bodyRef = document.querySelector('body');
 const menuItem = document.querySelector('ul');
 const changeThemeInput = document.querySelector('.theme-switch__toggle');
 
 const arrayItem = arrayMenu.map(menuElTpl).join('');
 menuItem.insertAdjacentHTML('beforeend', arrayItem);
 
-bodyRef.classList.add(theme.LIGHT);
 localStorageTheme();
 
 changeThemeInput.addEventListener('click', addClassTheme);
