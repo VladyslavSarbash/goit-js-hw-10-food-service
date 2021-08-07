eef64bdb11020493779882c31c3ba99243d6d277
VladyslavSarbash <79258970+VladyslavSarbash@users.noreply.github.com>
2021-08-07 22:33:17 +0300

    'HW'

---
+++ src/js/cheangeTheme.js
@@ -0,0 +1,16 @@
+const changeThemeInput = document.querySelector('.theme-switch__toggle');
+const bodyRef = document.querySelector('body');
+import theme from "../theme.json";
+
+export default function addClassTheme() {
+
+  if (changeThemeInput.checked) {
+    bodyRef.classList.remove(theme.LIGHT);
+    bodyRef.classList.add(theme.DARK);
+    localStorage.setItem('theme', theme.DARK)
+  } else {
+    bodyRef.classList.remove(theme.DARK);
+    bodyRef.classList.add(theme.LIGHT);
+    localStorage.setItem('theme', theme.LIGHT)
+  }
+}
---
+++ src/js/localStorageTheme.js
@@ -0,0 +1,14 @@
+const bodyRef = document.querySelector('body');
+const changeThemeInput = document.querySelector('.theme-switch__toggle');
+import theme from "../theme.json";
+
+
+export default function localStorageTheme() {
+  if (localStorage.getItem('theme') === theme.DARK) {
+    changeThemeInput.checked = true;
+    bodyRef.classList.remove(theme.LIGHT);
+    bodyRef.classList.add(theme.DARK);
+  } else {
+    changeThemeInput.checked = false;
+  }
+}
